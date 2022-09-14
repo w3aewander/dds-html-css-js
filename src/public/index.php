51c50db889d82a98b13506e2eaf933f3efb9bdbd
Wanderlei Silva do Carmo <wander.silva@gmail.com>
2022-09-14 17:32:55 -0300

    Primeiro Commit

---
+++ src/public/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aprendendo a criar WEB Pages dinâmicas</title>
+</head>
+<body>
+    <nav>
+        <a href=""   data-target="inicio.html" > Inicio </a> |
+        <a href="" data-target="conteudo.html"> Conteúdo</a> |
+        <a href=""  data-target="sobre.html"> Sobre </a> 
+    </nav>
+
+    <div id="app"></div>
+
+    <script src="js/app.js"></script>
+    
+</body>
+</html>
---
+++ src/public/js/app.js
@@ -0,0 +1,26 @@
+const app = document.getElementById('app')
+const ancora = document.querySelectorAll('a')
+
+ancora.forEach( a => {
+    a.addEventListener('click', (evt) =>{
+         evt.preventDefault()
+       
+         const  pagina = evt.target.getAttribute('data-target')
+
+         console.log(pagina)
+
+         fetch(pagina)
+         .then(resp => resp.text())
+         .then( resp => app.innerHTML = resp)
+
+    })
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+
+
+    
+
+    app.innerHTML = "Página dinamicamente carregada..."
+
+})
