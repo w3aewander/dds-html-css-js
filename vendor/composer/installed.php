3bf299128af92237b6c7d222cdd16f80c6b77e25
Wanderlei Silva WS <wander.silva@labcetec.net>
2022-09-19 15:51:04 -0300

    Adicionado formulário de contato

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wanderlei/dds-html-js-css',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'f71ad428c93d9676ef858981802f1e45d831d49e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wanderlei/dds-html-js-css' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'f71ad428c93d9676ef858981802f1e45d831d49e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
