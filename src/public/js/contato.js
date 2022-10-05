c83f6445eb6500d2c1fc4aaf87c146c5f6977a5e
w3aewander <wander.silva@gmail.com>
2022-10-05 17:41:33 -0300

    Iniciando o CRUD via ajax.

--- src/public/js/contato.js
+++ src/public/js/contato.js
@@ -2,6 +2,11 @@
  *  Contatos.
  */
 
+const popularForm = (elem) => {
+  const ct = elem.parentNode.parentNode
+  console.log(ct.getAttribute('data-nome'))
+}
+
 const obterContatos = () => {
     
     const tbContatos = document.getElementById('tb-contatos')
@@ -16,12 +21,23 @@ const obterContatos = () => {
 
         json.data.forEach( (e) => {
             console.log(e.id)
-            html += `<tr>
+            html += `<tr data-id="${e.id}" data-nome="${e.nome}" 
+                         data-email="${e.email}" data-assunto="${e.assunto}" 
+                         data-mensagem="${e.mensagem}">
+
                         <td>${e.id}</td>
                         <td>${e.nome}</td>
                         <td>${e.email}</td>
                         <td>${e.assunto}</td>
                         <td>${e.mensagem}</td>
+                        <td>
+                           <button type="button" onclick="popularForm(this);"   class="btn btn-info btn-sm">
+                                <i class="fa fa-edit"></i>
+                            </button>
+                           <button type="button" class="btn btn-danger btn-sm">
+                                <i class="fa fa-trash"></i>
+                            </button>
+                        </td>
                     </tr>`
             
         })
