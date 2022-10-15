7ffe6502ae36adc95b78d87ca59a0894497dc2bb
w3aewander <wander.silva@gmail.com>
2022-10-15 09:07:59 -0300

    CRUD completo para contatos

--- src/public/contato.php
+++ src/public/contato.php
@@ -5,8 +5,12 @@
 
 require __DIR__ . '/../../vendor/autoload.php';
 
-header('Access-Control-Allow-Origin:*');
-header("Access-Control-Allow-Methods:*");
+header('Access-Control-Allow-Origin: *'); 
+header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+
 header('Content-Type: Application/json; charset=UTF-8');
 
 ini_set('display_errors', TRUE);
